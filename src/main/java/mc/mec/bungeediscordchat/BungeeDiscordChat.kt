70e87f03f6bcfa824c5259e9fccaa7ea6c08af3f
M-E-C <Mr_El_Capitan@outlook.jp>
2020-06-20 21:53:13 +0900

    initial commit

---
+++ src/main/java/mc/mec/bungeediscordchat/BungeeDiscordChat.kt
@@ -0,0 +1,13 @@
+package mc.mec.bungeediscordchat
+
+import net.md_5.bungee.api.plugin.Plugin
+
+class BungeeDiscordChat : Plugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
